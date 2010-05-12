a265e0671c6a292fb3c4e863ccfa73d37c53e4df
sunspider <sunspider@9c93ef92-7051-0410-9388-5182e6f2e01c>
2010-05-12 18:21:49 +0000

    Delete Example_Project again

--- trunk/Example_Project/_pug_settings.py
+++
@@ -1,7 +0,0 @@
-### pug_settings autocode ###
-class pug_settings():
-    initial_scene = 'PigScene'
-    rect_Pig_Editor = (807, 4, 781, 670)
-    rect_frame = (100, 100, 400, 250)
-    save_settings_on_quit = True
-### End pug_settings autocode ###
