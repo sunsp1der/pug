c3db2565a86a4429b5fa93011da3333f681b3331
sunspider@gmail.com <sunspider@gmail.com@9c93ef92-7051-0410-9388-5182e6f2e01c>
2010-12-16 08:25:17 +0000

    icons for new component buttons

--- Pig_Demo/_pug_settings.py
+++ Pig_Demo/_pug_settings.py
@@ -1,7 +1,7 @@
 ### pug_settings autocode ###
 class pug_settings():
     Rect_Component_Browser = (119, 290, 633, 455)
-    Rect_Pig_Editor = (-5, 462, 853, 417)
+    Rect_Pig_Editor = (79, 437, 658, 405)
     Rect_Pug_Python_Editor = (0, 100, 869, 600)
     initial_scene = 'Dragon'
     rect_opioid_window = (10, 10, 800, 600)
