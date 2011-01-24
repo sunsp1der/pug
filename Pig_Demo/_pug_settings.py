c001f21a2015603e8b53addd5e58b97bbf693f87
sunspider <sunspider@9c93ef92-7051-0410-9388-5182e6f2e01c>
2011-01-24 00:02:04 +0000

    Release 0.8.2 clean up files

--- Pig_Demo/_pug_settings.py
+++
@@ -1,9 +0,0 @@
-### pug_settings autocode ###
-class pug_settings():
-    Rect_Component_Browser = (119, 290, 633, 455)
-    Rect_Pig_Editor = (435, 433, 658, 592)
-    Rect_Pug_Python_Editor = (129, 464, 800, 407)
-    initial_scene = 'MyScene'
-    rect_opioid_window = (9, 7, 800, 600)
-    save_settings_on_quit = True
-### End pug_settings autocode ###
